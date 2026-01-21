fd4eda3979dd0c41d99dea28ce915dca54f512bf
Kr0issant <93475036+Kr0issant@users.noreply.github.com>
2026-01-21 23:07:03 +0530

    add tic tac toe

---
+++ src/scripts/tic-tac-toe.js
@@ -0,0 +1,88 @@
+var chance = "cross";
+var move = 0;
+var gameEnded = false;
+
+const crossScore = document.querySelector(".score-cross");
+const circleScore = document.querySelector(".score-circle");
+
+const winMasks = [448, 56, 7, 292, 146, 73, 273, 84];
+var boxes = ["0", "0", "0", "0", "0", "0", "0", "0", "0"];
+
+const docBoxes = document.querySelectorAll(".tic-box");
+docBoxes.forEach((box) => {
+    box.addEventListener("click", ticClick);
+});
+
+const resetBtn = document.querySelector(".reset-btn");
+resetBtn.addEventListener("click", reset);
+
+function ticClick(e) {
+    if (e.target.innerHTML !== "") { return; }
+    if (gameEnded) { return; }
+
+    boxes[e.target.id] = chance;
+
+    if (chance === "cross") {
+        e.target.innerHTML = '<img src="../assets/cross.png" draggable="false">';
+
+        chance = "circle";
+    } else {
+        e.target.innerHTML = '<img src="../assets/circle.png" draggable="false">';
+
+        chance = "cross";
+    }
+    
+    move++;
+    let victor = checkWin();
+
+    if (victor != "") {
+        gameEnded = true;
+        
+        if (victor === "cross") {
+            crossScore.innerHTML = parseInt(crossScore.innerHTML) + 1;
+        } else if (victor === "circle") {
+            circleScore.innerHTML = parseInt(circleScore.innerHTML) + 1;
+        }
+    }
+}
+
+function checkWin() {
+    if (move === 9) {
+        gameEnded = true;
+    }
+
+    let victor = "";
+    let lastChance = (chance == "cross" ? "circle" : "cross");
+    
+    let gameMask = parseInt(boxes.join("").replaceAll(lastChance, "1").replaceAll(chance, "0"), 2);
+
+    winMasks.forEach(win => {
+        if ((gameMask & win) == win) {
+            victor = lastChance;
+
+            let w = win.toString(2);
+            w = "0".repeat(9 - w.length) + w;
+
+            for (i = 0; i < w.length; i++) {
+                if (w[i] === "1") {
+                    document.getElementById(i).classList.add("win-tile");
+                }
+            };
+        }
+    });
+
+    return gameEnded ? "tie" : victor;
+}
+
+function reset() {
+    for (i = 0; i < 9; i++) {
+        let t = document.getElementById(i);
+        t.innerHTML = "";
+        t.classList.remove("win-tile");
+
+        boxes[i] = "0";
+    }
+    move = 0;
+    chance = "cross";
+    gameEnded = false;
+}
